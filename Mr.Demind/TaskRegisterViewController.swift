0d7a4fe62418d9f71d33a528adbaea97c639a258
kaito-a-bit <arakai0128@gmail.com>
2021-10-30 15:42:53 +0900

    added cancel function

--- Mr.Demind/TaskRegisterViewController.swift
+++ Mr.Demind/TaskRegisterViewController.swift
@@ -16,9 +16,34 @@ class TaskRegisterViewController: UIViewController {
     @IBOutlet weak var notificationAdditionButton: UIButton!
     @IBOutlet weak var descriptionTextView: UITextView!
     
+  
+    @IBAction func cancelButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+//    @IBAction func registerButton(_ sender: Any) {
+//    }
+    var stateForPublishedDate: DayOfTheWeek?
+    var stateForViewingDeadline: DayOfTheWeek?
+    var stateForAssignmentDeadline: DayOfTheWeek?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         classTitleTextField.delegate = self
+    }
+    
+    func taskAddition() {
+        if let classTitle = classTitleTextField.text,
+           let stateForPublishedDate = stateForPublishedDate,
+           let stateForViewingDeadline = stateForViewingDeadline,
+           let stateForAssignmentDeadline = stateForAssignmentDeadline {
+            ClassListViewController.itemsForClassTableView.append(
+                registeredItems(classTitle: classTitle,
+                                published_Date: stateForPublishedDate,
+                                viewing_Deadline: stateForViewingDeadline,
+                                assignment_Deadline: stateForAssignmentDeadline,
+                                description: descriptionTextView.text))
+        }
     }
 }
 
