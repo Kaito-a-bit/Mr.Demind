67a0a4c23e2e6c05955c60b40622ac3965ed6fa0
kaito-a-bit <arakai0128@gmail.com>
2021-11-15 12:33:21 +0900

    created new file, and added property to registeredItems.

--- Mr.Demind/AppStruct.swift
+++ Mr.Demind/AppStruct.swift
@@ -30,6 +30,7 @@ struct registeredItems: Codable {
     var arrForButtons: [Int]
     var description: String?
     var ToggledDates: ToggledDates
+    var NotificationDates: [DateComponents]
 }
 
 struct ToggledDates: Codable {
---
+++ Mr.Demind/Notification.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.swift
+//  Mr.Demind
+//
+//  Created by 荒井海斗 on 2021/11/15.
+//
+
+import Foundation
+
+class Notification {
+    
+}
